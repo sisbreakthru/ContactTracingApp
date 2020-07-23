f02122bf06b40044dfca7bb781a6504bc338d946
Linda Smith <sisbreakthru@comcast.net>
2020-07-23 19:59:03 -0400

    Intro to Express Part 2

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express(); // returns an express server application
 
+app.use(morgan('dev')); // logs request info
+
+// set up express to serve static files
+// std behavior for webserver to serve up the index.html file if send req to hostname
+app.use(express.static(__dirname + '/public')); // __dirname = absolute path
+
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
